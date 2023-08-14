2bcff99e71a643ce789809c18b519782652f8407
Roman Grankov <grankov.r@promail366.com>
2023-08-14 23:29:30 +0300

    подключиться к облаку монго, добавить модели user и token

---
+++ server/models/token-model.ts
@@ -0,0 +1,22 @@
+import { Schema, model } from 'mongoose';
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isActivated: {
+        type: Boolean,
+        default: false,
+    },
+    activatedLink: {
+        type: String,
+    },
+});
+
+module.exports = model('User', UserSchema);
---
+++ server/models/user-model.ts
@@ -0,0 +1,14 @@
+import { Schema, model } from 'mongoose';
+
+const TokenSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    refreshToken: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = model('Token', TokenSchema);
