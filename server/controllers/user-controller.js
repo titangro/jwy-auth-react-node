a4a0e35770a770fb387036a387ca5a8970607e5e
Roman Grankov <grankov.r@promail366.com>
2023-08-15 00:03:43 +0300

    add user controller empty methods

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,34 @@
+class UserController {
+    async registration(req, res, next) {
+        try {
+        } catch (error) {}
+    }
+
+    async login(req, res, next) {
+        try {
+        } catch (error) {}
+    }
+
+    async logout(req, res, next) {
+        try {
+        } catch (error) {}
+    }
+
+    async activate(req, res, next) {
+        try {
+        } catch (error) {}
+    }
+
+    async refresh(req, res, next) {
+        try {
+        } catch (error) {}
+    }
+
+    async getUsers(req, res, next) {
+        try {
+            res.json([1, 243, 432]);
+        } catch (error) {}
+    }
+}
+
+module.exports = new UserController();
--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,4 +1,4 @@
-import { Schema, model } from 'mongoose';
+const { Schema, model } = require('mongoose');
 
 const TokenSchema = new Schema({
     user: {
