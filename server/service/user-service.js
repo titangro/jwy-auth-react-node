613a7e5a930dc0e1f2dd9c069cf6eb40315f87d5
Roman Grankov <grankov.r@promail366.com>
2023-08-16 23:51:40 +0300

    завершить фукнцию registration в  user service

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const mailService = require('../service/mail-service');
 const tokenService = require('../service/token-service');
+const UserDto = require('../dtos/user-dto');
 
 const salt = 3;
 
@@ -18,13 +19,22 @@ class UserService {
         }
         const hashPassword = await bcrypt.hash(password, salt);
         const activationLink = uuid.v4();
+
         const user = await UserModel.create({
             email,
             password: hashPassword,
             activationLink,
         });
         await mailService.sendActivationMail(email, activationLink);
-        const tokens = tokenService.generateTokens();
+
+        const userDto = new UserDto(user); // id, emeail, isActivated
+        const tokens = tokenService.generateTokens({ ...userDto });
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {
+            ...tokens,
+            user: userDto,
+        };
     }
 }
 
